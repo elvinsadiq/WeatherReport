11b0ee495a0836730947f99780e92e322047d46f
elvinsadiq <74909791+elvinsadiq@users.noreply.github.com>
2024-01-23 10:24:37 +0400

    DistrictId migration fix

---
+++ Infrastructure/Migrations/20240123062400_DistrictIdFix.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class DistrictIdFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeatherReport_Districts_DistrictId1",
+                table: "WeatherReport");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WeatherReport_DistrictId1",
+                table: "WeatherReport");
+
+            migrationBuilder.DropColumn(
+                name: "DistrictId1",
+                table: "WeatherReport");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DistrictId1",
+                table: "WeatherReport",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherReport_DistrictId1",
+                table: "WeatherReport",
+                column: "DistrictId1",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeatherReport_Districts_DistrictId1",
+                table: "WeatherReport",
+                column: "DistrictId1",
+                principalTable: "Districts",
+                principalColumn: "Id");
+        }
+    }
+}
